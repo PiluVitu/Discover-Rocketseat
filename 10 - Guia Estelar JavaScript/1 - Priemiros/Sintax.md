32c4c4c912bf6030050ffc48b65b3662262accc8
Paulo Victor Torres Silva <paulo.tspi@gmail.com>
2022-01-19 21:59:17 -0300

    js parado por hoje

---
+++ 10 - Guia Estelar JavaScript/1 - Priemiros/Sintax.md
@@ -0,0 +1,9 @@
+# A importância da Sintaxe
+
+- Toda linguagem tem
+- Uma boa comunicção necessita de uma boa sintaxe
+- 82% dos erros para iniciantes em progamação
+
+```js
+console.log('Bem vindo ao Starter')
+```
---
+++ 10 - Guia Estelar JavaScript/scripts.js
@@ -0,0 +1,8 @@
+// Aqui eu tenho um comentario em linha
+
+// O codigo aqui em baixo printa algo no dev tools
+console.log('Bem Vindos ao Starter!!!!!')
+
+/* Se vier uma (/*) antes é um comentario 
+
+console.log('Bem Vindos ao Stater!!!!!')*/
