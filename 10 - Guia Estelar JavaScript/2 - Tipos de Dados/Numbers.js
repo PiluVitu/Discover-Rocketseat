493d55c5f2f379bf56831e92288c42b9249d4eba
Paulo Victor Torres Silva <paulo.tspi@gmail.com>
2022-01-28 17:18:15 -0300

    Numbers end

---
+++ 10 - Guia Estelar JavaScript/2 - Tipos de Dados/Numbers.js
@@ -0,0 +1,18 @@
+/* 
+  Number
+    * Números
+
+    33 // inteiros
+    12.5 // reais - float
+    NaN // Not a Number
+    Infinity // infinito
+
+*/
+
+console.log(33 + 12.5)
+
+console.log(12.9 - 'pily') // Imprime NaN
+
+console.log(12.6 == Infinity) // Ele vai verificar se é infinito
+
+console.log(Infinity)
