f28dc089d4c6f82153d6d68f12b2a4310874ddaf
Paulo Victor Torres Silva <paulo.tspi@gmail.com>
2022-01-28 17:35:56 -0300

    Object done

---
+++ 10 - Guia Estelar JavaScript/2 - Tipos de Dados/Object.js
@@ -0,0 +1,16 @@
+/*
+ * Object
+    * Objeto > Dado estrutural 
+    * Propriedades / Atributos
+    * Funcionalidades / Métodos
+  
+  { propriedade: "valor" }
+ */
+
+console.log({
+  name: 'Pilu Vitu',
+  idade: 36,
+  andar: function () {
+    console.log('andar')
+  }
+})
