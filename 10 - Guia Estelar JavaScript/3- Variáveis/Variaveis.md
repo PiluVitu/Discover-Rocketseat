0cf454350ac5294fb308ea6df0cddf25c37d318d
Paulo Victor Torres Silva <paulo.tspi@gmail.com>
2022-01-31 17:52:13 -0300

    variaveis start

---
+++ 10 - Guia Estelar JavaScript/3- Variáveis/Variaveis.md
@@ -0,0 +1,12 @@
+# Variáveis
+
+- Nomes simbólicos para receber algum valor
+
+- Atalho de código
+
+- Indentificadores
+
+- 3 palavras reservadas para criar uma variavel
+  - var
+  - let
+  - cosnt
--- 10 - Guia Estelar JavaScript/scripts.js
+++ 10 - Guia Estelar JavaScript/scripts.js
@@ -1,1 +1,13 @@
+// Tipos de variáveis
+/**/
+// var
+var clima = 'Mormaço'
+clima = 'Ameno' //se atribuir uma novo resultado para clima, vale o ultimo
 
+//let
+let carro = 'kled'
+
+//const
+const lápis = 'Faber Castel'
+
+console.log(clima, carro, lápis)
